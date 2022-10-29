50f42ae7eec240ee42c4a363c835046a8ae2bc3c
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-10-29 19:50:45 +0600

    Added Library class

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 class User:
+    allUsers = []
     def __init__(self,name,roll,password) -> None:
         self.name = name
         self.roll = roll
         self.password = password
         self.borrow_books = []
         self.returned_books = []
+
+class Library:
+    def __init__(self,book_list) -> None:
+        self.book_list = book_list
+
+
+library = Library({"English":2,"Bangla":5,"Math":3})
+allUsers = []
+currentUser = None
